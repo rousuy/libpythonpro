96ca12c7ba5c96ce248a3aa54bba9d9517dba630
Rodrigo Usuy <rousuy@gmail.com>
2021-02-23 19:08:10 -0300

    Nova correção de nome do modulo para publicação no PyPi

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro_dihl"
+PACKAGE = "dihllibpythonpro"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Marcos Dihl"
